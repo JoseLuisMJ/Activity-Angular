48eb107b2462208fb961d17b652759ad427bf531
JoseLuisMJ <trepa01@hotmail.com>
2023-09-25 20:45:22 +0200

    add housing-location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Views/home/home.component';
+import { HousingLocationComponent } from './views/housing-location/housing-location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HousingLocationComponent
   ],
   imports: [
     BrowserModule,
